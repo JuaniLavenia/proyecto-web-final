27c5a0a431417f0b9fed48be737072d45d21b43e
Dario Alba <darioalba0@gmail.com>
2023-04-15 14:09:00 -0300

    se trabaja sobre edit

--- src/pages/admin/Create.jsx
+++ src/pages/admin/Create.jsx
@@ -1,5 +1,49 @@
 function Create() {
-  return <>Soy el Create</>;
+  return (
+    <div className="container">
+      <h1>Crear Producto</h1>
+
+      <form>
+        <div className="mb-3">
+          <label htmlFor="nombre" className="form-label">
+            Nombre:
+          </label>
+          <input
+            type="text"
+            name="nombre"
+            id="nombre"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="stock" className="form-label">
+            Stock:
+          </label>
+          <input
+            type="number"
+            name="stock"
+            id="stock"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="descripcion" className="form-label">
+            Descripción:
+          </label>
+          <textarea
+            name="descripcion"
+            id="descripcion"
+            cols="30"
+            rows="10"
+            className="form-control"
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Guardar
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Create;
