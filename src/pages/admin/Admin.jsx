c91195d51b696f14c78bca68d6a2747727f0662f
Dario Alba <88122951+Darioalba@users.noreply.github.com>
2023-04-18 09:27:06 -0300

    se trabaja sobre edit

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function Admin() {
+  const { id } = useParams()
   //hook para iniciar la lista
   const [productos, setProductos] = useState([]);
   //hook para inicio de paginacion
@@ -9,16 +10,16 @@ function Admin() {
 
   //hook para listar los productos de la base de datos
   useEffect(() => {
-    fetch(`http://localhost:3000/crud?_page=${page}&_limit=5`)
+    fetch(`http://localhost:3000/productos?_page=${page}&_limit=5`)
       .then((response) => response.json())
       .then((json) => setProductos(json));
   }, []);
 
   //paginacion de productos en crud
   const paginacion = (page) => {
-    if(page <= 0) return
-    if(page >= 4) return
-    fetch(`http://localhost:3000/crud?_page=${page}&_limit=5`)
+    if (page <= 0) return
+    if (page >= 4) return
+    fetch(`http://localhost:3000/productos?_page=${page}&_limit=5`)
       .then((response) => response.json())
       .then((json) => setProductos(json))
     setPage(page)
@@ -29,10 +30,9 @@ function Admin() {
   return (
     <div className="container">
       <h1 className="text-center">Lista de productos</h1>
-      <Link to="/create" className="btn btn-primary float-end">
+      <Link to="/producto/create" className="btn btn-primary float-end">
         Crear
       </Link>
-
 
       {/* tabla de productos */}
       <table class="table table-striped">
@@ -50,14 +50,15 @@ function Admin() {
           {productos.map((producto) => (
             <tr key={producto.id}>
               <td scope="row">{producto.id}</td>
-              <td scope="row">{producto.producto}</td>
+              <td scope="row">
+                <Link to={`/productos/${producto.id}`}>
+                  {producto.nombre}
+                </Link></td>
               <td scope="row">{producto.nombre}</td>
               <td scope="row">{producto.stock}</td>
               <td scope="row">{producto.descripcion}</td>
               <td className="text-end">
-                <Link className="btn btn-warning" to="/edit">
-                  Editar
-                </Link>
+                <Link className="btn btn-info float-end" to={`/producto/${producto.id}/edit`}>Editar</Link>
                 <Link className="btn btn-danger">Borrar</Link>
               </td>
             </tr>
@@ -67,8 +68,8 @@ function Admin() {
 
       {/* botones de paginacion */}
       <div className="container text-center">
-        <button className="btn btn-light" onClick={() =>paginacion(page - 1)}>Anterior</button>
-        <button className="btn btn-light" onClick={() =>paginacion(page + 1)}>Siguiente</button>
+        <button className="btn btn-light" onClick={() => paginacion(page - 1)}>Anterior</button>
+        <button className="btn btn-light" onClick={() => paginacion(page + 1)}>Siguiente</button>
       </div>
 
     </div>
