a9199dcaf6e34057368d05b3c012e1de8947da2f
Juani Lavenia <juanilavenia34@gmail.com>
2023-04-22 23:54:43 -0300

    agregue funcionalidad

--- src/components/Formcarrito.jsx
+++ src/components/Formcarrito.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 
 function Formcarrito(props) {
   const [cardNumber, setCardNumber] = useState("");
-  const [cardExpiration, setCardExpiration] = useState("");
+  const [cardExpirationMonth, setCardExpMonth] = useState("");
+  const [cardExpirationYear, setCardExpYear] = useState("");
   const [cardCvv, setCardCvv] = useState("");
   const [shippingName, setShippingName] = useState("");
   const [shippingAddress, setShippingAddress] = useState("");
+  const [formValid, setFormValid] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -15,55 +17,185 @@ function Formcarrito(props) {
     props.history.push("/confirmacion-pedido");
   };
 
+  const validateForm = () => {
+    const form = document.querySelector(".formcarrito");
+    const inputs = form.querySelectorAll("input");
+    let isValid = true;
+
+    inputs.forEach((input) => {
+      if (!input.value) {
+        isValid = false;
+      }
+    });
+
+    setFormValid(isValid);
+    localStorage.setItem("validation", isValid);
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Información de pago</h2>
-      <label htmlFor="cardNumber">Número de tarjeta de crédito</label>
-      <input
-        type="text"
-        id="cardNumber"
-        value={cardNumber}
-        onChange={(e) => setCardNumber(e.target.value)}
-        required
-      />
+    <form onSubmit={handleSubmit} className="form-container formcarrito">
+      <div className="form-group">
+        <h2 className="form-label">Información de pago</h2>
+        <div>
+          <label htmlFor="cardNumber" className="form-label">
+            Tipo de tarjeta
+          </label>
+          <select
+            className="form-select"
+            aria-label="Default select example"
+            required
+          >
+            <option>Seleccione la entidad</option>
+            <option value="1">Visa</option>
+            <option value="2">MasterCard</option>
+          </select>
+          <div className="invalid-feedback">
+            Por favor, completa este campo.
+          </div>
+        </div>
 
-      <label htmlFor="cardExpiration">Fecha de vencimiento (MM/AA)</label>
-      <input
-        type="text"
-        id="cardExpiration"
-        value={cardExpiration}
-        onChange={(e) => setCardExpiration(e.target.value)}
-        required
-      />
+        <div>
+          <label htmlFor="cardNumber" className="form-label">
+            Número de la tarjeta
+          </label>
+          <input
+            className="form-control"
+            min={0}
+            minLength={13}
+            maxLength={18}
+            type="number"
+            id="cardNumber"
+            value={cardNumber}
+            onChange={(e) => {
+              const value = e.target.value;
+              if (value.length <= 18) {
+                setCardNumber(value);
+                validateForm();
+              }
+            }}
+            required
+          />
+          <div className="invalid-feedback">
+            Por favor, completa este campo.
+          </div>
+        </div>
 
-      <label htmlFor="cardCvv">Código de seguridad (CVV)</label>
-      <input
-        type="text"
-        id="cardCvv"
-        value={cardCvv}
-        onChange={(e) => setCardCvv(e.target.value)}
-        required
-      />
+        <div className="row">
+          <label htmlFor="cardExpiration" className="form-label">
+            Fecha de vencimiento (MM/AA)
+          </label>
+          <div className="form-group d-flex">
+            <input
+              className="form-control w-50"
+              type="number"
+              min={1}
+              max={12}
+              id="cardExpiration"
+              value={cardExpirationMonth}
+              onChange={(e) => {
+                const value = e.target.value;
+                if (value.length <= 2) {
+                  setCardExpMonth(value);
+                  validateForm();
+                }
+              }}
+              required
+            />
+            <strong className="align-self-center mx-2">/</strong>
+            <input
+              className="form-control w-50"
+              type="number"
+              min={23}
+              max={99}
+              id="cardExpirationYear"
+              value={cardExpirationYear}
+              onChange={(e) => {
+                const value = e.target.value;
+                if (value.length <= 2) {
+                  setCardExpYear(value);
+                  validateForm();
+                }
+              }}
+              required
+            />
+            <div className="invalid-feedback">
+              Por favor, completa este campo.
+            </div>
+          </div>
+        </div>
 
-      <h2>Información de envío</h2>
-      <label htmlFor="shippingName">Nombre completo</label>
-      <input
-        type="text"
-        id="shippingName"
-        value={shippingName}
-        onChange={(e) => setShippingName(e.target.value)}
-        required
-      />
+        <div>
+          <label htmlFor="cardCvv" className="form-label">
+            Código de seguridad (CVV)
+          </label>
+          <input
+            className="form-control"
+            min={0}
+            max={999}
+            maxLength={3}
+            minLength={3}
+            type="number"
+            id="cardCvv"
+            value={cardCvv}
+            onChange={(e) => {
+              const value = e.target.value;
+              if (value.length <= 3) {
+                setCardCvv(value);
+                validateForm();
+              }
+            }}
+            required
+          />
 
-      <label htmlFor="shippingAddress">Dirección de envío</label>
-      <textarea
-        id="shippingAddress"
-        value={shippingAddress}
-        onChange={(e) => setShippingAddress(e.target.value)}
-        required
-      />
+          <div className="invalid-feedback">
+            Por favor, completa este campo.
+          </div>
+        </div>
+      </div>
+      <div className="form-group">
+        <h2 className="form-label">Información de envío</h2>
+        <div>
+          <label htmlFor="shippingName" className="form-label">
+            Nombre completo
+          </label>
+          <input
+            className="form-control"
+            maxLength={40}
+            type="text"
+            id="shippingName"
+            value={shippingName}
+            onChange={(e) => {
+              setShippingName(e.target.value);
+              validateForm();
+            }}
+            required
+          />
+          <div className="invalid-feedback">
+            Por favor, completa este campo.
+          </div>
+        </div>
 
-      <button type="submit">Pagar y enviar</button>
+        <div>
+          <label htmlFor="shippingAddress" className="form-label">
+            Dirección de envío
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            maxLength={140}
+            id="shippingAddress"
+            value={shippingAddress}
+            onChange={(e) => {
+              setShippingAddress(e.target.value);
+              validateForm();
+            }}
+            required
+          />
+          <div className="invalid-feedback">
+            Por favor, completa este campo.
+          </div>
+        </div>
+      </div>
     </form>
   );
 }
--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,28 +1,58 @@
-import { Link } from "react-router-dom";
 import "./Carrito.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Formcarrito from "../components/Formcarrito";
+import { useNavigate } from "react-router";
 
 function Carrito() {
   const [showModal, setShowModal] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [formValid, setFormValid] = useState(false);
+  const valKey = localStorage.getItem("validation");
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem("cartItems"));
+    if (items) {
+      setCartItems(items);
+    }
+  }, []);
+
+  const calculateTotal = () => {
+    let total = 0;
+    cartItems.forEach((item) => {
+      total += item.price;
+    });
+    return total;
+  };
 
   const handleShowModal = (e) => {
     e.preventDefault();
-
     setShowModal(true);
   };
 
   const handleCloseModal = () => {
     setShowModal(false);
   };
+
+  const handleAddToCart = (producto) => {
+    setCartItems([...cartItems, producto]);
+    localStorage.setItem("cartItems", JSON.stringify([...cartItems, producto]));
+  };
+
+  const handleRemoveFromCart = (producto) => {
+    const newCartItems = cartItems.filter((item) => item.id !== producto.id);
+    if (newCartItems.length !== cartItems.length) {
+      setCartItems(newCartItems);
+      localStorage.setItem("cartItems", JSON.stringify(newCartItems));
+    }
+  };
+
   return (
-    <div className="main">
-      <div className="table-responsive">
+    <div className="main bg-dark text-light">
+      <div className="table-responsive ">
         {cartItems.length > 0 ? (
           <>
             <table
-              className="table table-sm text-center align-middle"
+              className="table table-sm text-center align-middle bg-dark text-light"
               id="table"
             >
               <thead>
@@ -34,24 +64,39 @@ function Carrito() {
                   <th scope="col">Eliminar</th>
                 </tr>
               </thead>
-              <tbody className="tbody" id="cartTable"></tbody>
+              <tbody className="tbody " id="cartTable">
+                {cartItems.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.id}</td>
+                    <td>{item.name}</td>
+                    <td>{item.price}</td>
+                    <td>1</td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => handleRemoveFromCart(item)}
+                      >
+                        Eliminar
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
             </table>
 
             <div className="col total-comprar">
               <h3 className="itemCardTotal" id="itemTotal">
-                Total $
+                Total ${calculateTotal()}
               </h3>
 
-              <Link to="">
-                <button
-                  className="btn btn-success"
-                  onClick={handleShowModal}
-                  data-bs-toggle="modal"
-                  data-bs-target="#exampleModal"
-                >
-                  COMPRAR
-                </button>
-              </Link>
+              <button
+                className="btn btn-success"
+                onClick={handleShowModal}
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                COMPRAR ({cartItems.length})
+              </button>
             </div>
           </>
         ) : (
@@ -62,9 +107,9 @@ function Carrito() {
       {showModal && (
         <div className="modal" tabIndex="-1" role="dialog" id="exampleModal">
           <div className="modal-dialog" role="document">
-            <div className="modal-content">
+            <div className="modal-content bg-dark">
               <div className="modal-header">
-                <h5 className="modal-title">Información de pago y envío</h5>
+                <h5 className="modal-title">Información de la compra</h5>
                 <button
                   type="button"
                   className="close"
@@ -79,7 +124,8 @@ function Carrito() {
               <div className="modal-footer">
                 <button
                   type="button"
-                  className="btn btn-secondary"
+                  className="btn btn-danger"
+                  data-bs-dismiss="modal"
                   onClick={handleCloseModal}
                 >
                   Cancelar
@@ -87,9 +133,18 @@ function Carrito() {
                 <button
                   type="button"
                   className="btn btn-primary"
-                  onClick={handleCloseModal}
+                  data-bs-dismiss="modal"
+                  onClick={() => {
+                    if (valKey) {
+                      alert("¡Compra realizada correctamente!");
+                      handleCloseModal();
+                      setCartItems([]);
+                      localStorage.removeItem("cartItems");
+                      localStorage.removeItem("validation");
+                    }
+                  }}
                 >
-                  Enviar
+                  Pagar y enviar
                 </button>
               </div>
             </div>
