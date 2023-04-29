5a30a95dda0890888f6cb6ed549289cf8de62097
Martin Moreno <tincho.m9111@gmail.com>
2023-04-29 10:19:02 -0300

    adhesion productos

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,11 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/producto" element={<Producto />} />
+        <Route path="/edit" element={<Edit />} />
+        <Route path="/create" element={<Create />} />
       </Routes>
+      <Productos />
 
       <Footer />
     </>
