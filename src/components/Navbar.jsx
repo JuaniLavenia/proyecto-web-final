68f6b0529e1dfdf430d568e5d90ffb34033b9fe7
Dario Alba <88122951+Darioalba@users.noreply.github.com>
2023-05-10 11:58:48 -0300

    cambio en ruta

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,7 +89,7 @@ function Navbar() {
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/productos">
+                  <Link className="nav-link" to="/adm/productos">
                     Administrador
                   </Link>
                 </li>
