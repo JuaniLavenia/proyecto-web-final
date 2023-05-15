01b0363d2b7bd26e91db64ad1656a1b8bfe7c8c4
Juani Lavenia <juanilavenia34@gmail.com>
2023-05-15 17:31:08 -0300

    hice responsive admin

--- src/pages/admin/Producto.jsx
+++ src/pages/admin/Producto.jsx
@@ -52,7 +52,7 @@ function Producto() {
   //Estado de buscador
 
   return (
-    <div className=" p-5 bg-dark text-light">
+    <div className="contentPrinc p-5 bg-dark text-light">
       <div className="list d-flex justify-content-between align-items-center">
         <h1 className="text-center">Lista de productos</h1>
         <Link to="/adm/productos/create" className="btn btn-primary float-end">
@@ -78,56 +78,58 @@ function Producto() {
         </button>
       </div>
 
-      <table className="table text-light ">
-        <thead className="thead">
-          <tr>
-            <th scope="col">Nombre</th>
-            <th scope="col">Categoria</th>
-            <th scope="col">Precio</th>
-            <th scope="col">Stock</th>
-            <th scope="col">Capacidad</th>
-            <th scope="col">Imagen</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody className="tbody">
-          {productos &&
-            productos.map((producto) => (
-              <tr key={producto._id} className="file">
-                <td scope="row">{producto.name}</td>
-                <td scope="row">{producto.category}</td>
-                <td scope="row">$ {producto.price}</td>
-                <td scope="row">{producto.stock}</td>
-                <td scope="row">{producto.capacity}</td>
-                <td scope="row">
-                  <img
-                    //ruta para buscar imagen
-                    src={`http://localhost:3000/img/productos/${producto.image}`}
-                    width={100}
-                    alt={producto.nombre}
-                    className="img"
-                  />
-                </td>
-                <td scope="row">
-                  <Link
-                    to={`/adm/productos/edit/${producto._id}`}
-                    type="button"
-                    className="btn btn-warning m-1"
-                  >
-                    Editar
-                  </Link>
-                  <button
-                    onClick={() => destroy(producto._id)}
-                    type="button"
-                    className="btn btn-danger m-1"
-                  >
-                    Borrar
-                  </button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      <div className="table-responsive">
+        <table className="table text-light tableBody">
+          <thead className="thead">
+            <tr>
+              <th scope="col">Nombre</th>
+              <th scope="col">Categoria</th>
+              <th scope="col">Precio</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Capacidad</th>
+              <th scope="col">Imagen</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody className="tbody">
+            {productos &&
+              productos.map((producto) => (
+                <tr key={producto._id} className="file">
+                  <td scope="row">{producto.name}</td>
+                  <td scope="row">{producto.category}</td>
+                  <td scope="row">$ {producto.price}</td>
+                  <td scope="row">{producto.stock}</td>
+                  <td scope="row">{producto.capacity}</td>
+                  <td scope="row">
+                    <img
+                      //ruta para buscar imagen
+                      src={`http://localhost:3000/img/productos/${producto.image}`}
+                      width={100}
+                      alt={producto.nombre}
+                      className="img"
+                    />
+                  </td>
+                  <td scope="row">
+                    <Link
+                      to={`/adm/productos/edit/${producto._id}`}
+                      type="button"
+                      className="btn btn-warning m-1"
+                    >
+                      Editar
+                    </Link>
+                    <button
+                      onClick={() => destroy(producto._id)}
+                      type="button"
+                      className="btn btn-danger m-1"
+                    >
+                      Borrar
+                    </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
