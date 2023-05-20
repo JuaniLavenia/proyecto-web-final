5fdcc39997d4647fa051bd8f51ee3cfcc4a41775
Martin Moreno <tincho.m9111@gmail.com>
2023-05-20 15:46:04 -0300

    integracion AboutUS

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -4,7 +4,7 @@ function AboutUs() {
   return (
     <>
       <div className="allContent">
-        <div className="card bg-dark text-light">
+        <div className="card border border-warning bg-dark text-light">
           <img
             src="https://blog.advesa.com/wp-content/uploads/2019/11/about-us-page.png"
             className="card-img-top imgAbout w-50 d-flex align-self-center mt-4"
@@ -22,7 +22,7 @@ function AboutUs() {
         </div>
 
         <div className="card-group row m-auto bg-dark text-light">
-          <div className="card p-3 bg-dark text-light">
+          <div className="card border border-warning p-3 bg-dark text-light">
             <img
               src="https://ca.slack-edge.com/THQU1MGPN-U03Q9HH12RZ-4dbc2382e13c-512"
               className="rounded-circle w-30 p-3 border border-warning card-img-top imgCard"
@@ -36,7 +36,7 @@ function AboutUs() {
             </div>
           </div>
 
-          <div className="card p-3 bg-dark text-light">
+          <div className="card border border-warning p-3 bg-dark text-light">
             <img
               src="https://ca.slack-edge.com/THQU1MGPN-U03PRCPB9FV-9d578752aec1-512"
               className="rounded-circle w-30 p-3 border border-warning card-img-top imgCard"
@@ -50,28 +50,28 @@ function AboutUs() {
             </div>
           </div>
 
-          <div className="card p-3 bg-dark text-light">
+          <div className="card border border-warning p-3 bg-dark text-light">
             <img
               src="https://ca.slack-edge.com/THQU1MGPN-U03N3DC1WCC-b0a574a511e2-512"
               className="rounded-circle w-30 p-3 border border-warning card-img-top imgCard"
               alt=""
             />
             <div className="card-body">
-              <h5 className="card-title text-center">Alba Dario</h5>
+              <h5 className="card-title text-center">Marcos Coronel</h5>
             </div>
             <div className="card-footer text-center">
               <small className="text-muted">Integrante</small>
             </div>
           </div>
 
-          <div className="card p-3 bg-dark text-light">
+          <div className="card border border-warning p-3 bg-dark text-light">
             <img
               src="https://ca.slack-edge.com/THQU1MGPN-U046N12M1A7-g799cff9e206-512"
               className="rounded-circle w-30 p-3 border border-warning card-img-top imgCard"
               alt=""
             />
             <div className="card-body">
-              <h5 className="card-title text-center">Moreno Martin</h5>
+              <h5 className="card-title text-center">Marcos Coronel</h5>
             </div>
             <div className="card-footer text-center">
               <small className="text-muted">Integrante</small>
