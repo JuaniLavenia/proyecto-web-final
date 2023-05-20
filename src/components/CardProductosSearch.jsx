8ca92273adc2df654e6fae617b48beabf04157c7
Martin Moreno <tincho.m9111@gmail.com>
2023-05-20 10:57:03 -0300

    CardProductosSearch

--- src/components/CardProductosSearch.jsx
+++ src/components/CardProductosSearch.jsx
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import "./CardProductosSearch.css";
-import Swal from "sweetalert2";
+import { Button, Card } from "react-bootstrap";
 
 function CardProductos({
   image,
@@ -10,84 +10,87 @@ function CardProductos({
   category,
   _id,
   price,
-  setCartCount,
-  setFavoritesCount,
 }) {
+  const [cartCount, setCartCount] = useState(0);
+  const [favoritesCount, setFavoritesCount] = useState(0);
+
+  const updateLocalStorageCount = (key, count) => {
+    localStorage.setItem(key, count);
+    if (key === "cartCount") {
+      setCartCount(count);
+    } else if (key === "favoritesCount") {
+      setFavoritesCount(count);
+    }
+  };
+
   const handleAddToCart = (product) => {
+    // Primero, obtén el carrito actual del almacenamiento local (si lo hay).
     let cart = JSON.parse(localStorage.getItem("cartItems")) || [];
 
+    // Luego, verifica si el producto ya está en el carrito.
     let existingProduct = cart.find((p) => p._id === product._id);
 
     if (existingProduct) {
+      // Si el producto ya está en el carrito, actualiza su cantidad.
       existingProduct.quantity += 1;
     } else {
+      // Si el producto no está en el carrito, agrega el producto al carrito con una cantidad de 1.
       cart.push({ ...product, quantity: 1 });
     }
 
+    // Finalmente, guarda el carrito actualizado en el almacenamiento local.
     localStorage.setItem("cartItems", JSON.stringify(cart));
-    Swal.fire({
-      position: "top-center",
-      icon: "success",
-      title: "Se agregó el producto al carrito",
-      showConfirmButton: false,
-      timer: 1500,
-    });
+    alert("Se agrego al carrito!");
 
-    const cartCount = cart.reduce((count, item) => count + item.quantity, 0);
-
-    const updatedCartCount = cartCount;
-    setCartCount(updatedCartCount);
+    const count = cart.length;
+    updateLocalStorageCount("cartCount", count);
   };
 
   const addFavorite = (favorite) => {
+    // Primero, obtén el carrito actual del almacenamiento local (si lo hay).
     let fav = JSON.parse(localStorage.getItem("favItems")) || [];
 
+    // Luego, verifica si el producto ya está en el carrito.
     let existingProduct = fav.find((p) => p._id === favorite._id);
 
     if (existingProduct) {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "El producto ya existe en favoritos",
-      });
+      // Si el producto ya está en el carrito, actualiza su cantidad.
+      alert("El producto ya existe en favoritos");
     } else {
+      // Si el producto no está en el carrito, agrega el producto al carrito con una cantidad de 1.
+
       fav.push({ ...favorite, quantity: 1 });
       localStorage.setItem("favItems", JSON.stringify(fav));
-      Swal.fire({
-        position: "top-center",
-        icon: "success",
-        title: "Se agregó a favoritos",
-        showConfirmButton: false,
-        timer: 1500,
-      });
+      alert("Se agrego a favoritos!");
     }
 
-    const favoritesCount = fav.length;
-    const updatedFavCount = favoritesCount;
-    setFavoritesCount(updatedFavCount);
+    const count = fav.length;
+    updateLocalStorageCount("favoritesCount", count);
   };
 
   return (
-    <div className="card cardP m-3 bg-dark text-light d-flex">
-      <img
+    <Card className="cardP m-3 bg-dark text-light d-flex">
+      <Card.Img
+        variant="top"
+        src={`http://localhost:3000/img/productos/${image}`}
         className="imgCard d-flex"
-        src={`https://proyecto-web-final-backend--juan-ignacio245.repl.co/img/productos/${image}`}
         alt={name}
       />
-      <div className="card-body">
-        <h5 className="card-title">{name}</h5>
-        <p className="card-text">{description}</p>
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{description}</Card.Text>
         <div className="card-text d-flex pt-3">
           <p className="text-muted text-center w-50">{capacity}</p>
           <strong className="text-muted align-self-center pb-3">|</strong>
           <p className="text-muted text-center w-50">{category}</p>
         </div>
-      </div>
-      <div className="card-footer">
+      </Card.Body>
+      <Card.Footer>
         <div className="card-price p-1">$ {price}</div>
         <div className="car-buttons d-flex justify-content-center">
-          <button
-            className="btn btn-primary me-2"
+          <Button
+            variant="primary"
+            className="me-2"
             onClick={() => {
               handleAddToCart({
                 image,
@@ -101,9 +104,9 @@ function CardProductos({
             }}
           >
             Agregar al carrito
-          </button>
-          <button
-            className="btn btn-warning"
+          </Button>
+          <Button
+            variant="warning"
             onClick={() => {
               addFavorite({
                 image,
@@ -117,10 +120,10 @@ function CardProductos({
             }}
           >
             Agregar a favoritos
-          </button>
+          </Button>
         </div>
-      </div>
-    </div>
+      </Card.Footer>
+    </Card>
   );
 }
 export default CardProductos;
